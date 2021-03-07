770f3206f0812d5440f88c4fb8d90458343f8e04
giacomo9999 <gmarchesi888@gmail.com>
2021-03-07 18:16:06 -0500

    Moved data to App component.

--- rt-client/src/App.js
+++ rt-client/src/App.js
@@ -27,15 +27,65 @@ function App() {
       ))
     : null;
 
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      wv: 3200,
+      // amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      wv: 4300,
+      // amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 15000,
+      wv: 7600,
+      // amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      wv: 1900,
+      // amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      wv: 4500,
+      // amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      wv: 500,
+      // amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      wv: 1800,
+      // amt: 2100,
+    },
+  ];
+
   return (
     <div className="container-outer">
       <h1>Visualizing Twitter</h1>
       <div className="graph-container">
-        <DataGraph />
+        <DataGraph graphData={data}/>
       </div>
-      <button onClick={() => fetchData("erikloomis", 4)}>
-        Fetch The Data
-      </button>
+      <button onClick={() => fetchData("erikloomis", 4)}>Fetch The Data</button>
       <ul>{dataDisplay}</ul>
     </div>
   );
